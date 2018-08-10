eeb56e07b699722c7eacb57904909dbfd586d328
Preston Droschak <prestondroschak@Prestons-MacBook-Pro.local>
2018-08-10 13:54:23 -0400

    edited code

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 from flask import Flask, render_template, jsonify, redirect
-import pymongo
+from flask_pymongo import PyMongo
 import scrape_mars
 
 app = Flask(__name__)
 
-client = pymongo.MongoClient()
-db = client.mars_db
-collection = db.mars_data_entries
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mission_to_mars"
+mongo = PyMongo(app)
 
-@app.route("/")
-def home():
-    mars_data = list(db.collection.find())[0]
-    return  render_template('index.html', mars_data=mars_data)
+@app.route('/')
+def index():
+	mission_to_mars = mongo.db.mission_to_mars.find_one()
+	return render_template("index.html", mission_to_mars=mission_to_mars)
 
-@app.route("/scrape")
-def web_scrape():
-    db.collection.remove({})
-    mars_data = scrape_mars.scrape()
-    db.collection.insert_one(mars_data)
-    return  render_template('index.html')
+@app.route('/scrape')
+def scrape():
+	mission_to_mars = mongo.db.mission_to_mars
+	mission_to_mars_data = scrape_mars.scrape()
+	mission_to_mars.update(
+		{},
+		mission_to_mars_data,
+		upsert=True
+	)
+	return redirect("/",code=302)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,137 +1,101 @@
 import time
+import requests
 import pandas as pd
 from splinter import Browser
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 def init_browser():
     executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
     return Browser('chrome', **executable_path, headless=False)
 
+mission_to_mars = {}
+
 def scrape ():
     
     browser = init_browser()
-    nasa_data = {}
 
     nasa_url = 'https://mars.nasa.gov/news/'
     browser.visit(nasa_url)
     nasa_html = browser.html
     nasa_soup = BeautifulSoup(nasa_html, 'html.parser')
 
-    nasa_data["nasa_headline"] = nasa_soup.find("div", class_="content-title").get_text()
-    nasa_data["nasa_teaser"] = nasa_soup.find("div", class_="article-teaser-body").get_text()
+    title = nasa_soup.find("div", class_="content-title").text()
+    mission_to_mars["headline"] = title
 
-    return nasa_data
+    paragraph = nasa_soup.find("div", class_="article-teaser-body").text()
+    mission_to_mars["article"] = paragraph
 
-def scrape ():
 
     browser = init_browser()
-    jpl_data = {}
-
     jpl_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(jpl_url)
     jpl_html = browser.html
     jpl_soup = BeautifulSoup(jpl_html, 'html.parser')
+    image_results = jpl_soup.find('img', class_='thumb')
+    image_src = image_results['src']
+    featured_image = 'https://www.jpl.nasa.gov/' + image_src
 
-    browser.click_link_by_partial_text('FULL IMAGE')
-    try:
-        expand = browser.find_by_css('a.fancybox-expand')
-        expand.click()
+    mission_to_mars['featured_image'] = featured_image
 
-        img_relative = jpl_soup.find('img', class_='fancybox-image')['src']
-        image_path = f'https://www.jpl.nasa.gov{img_relative}'
-        jpl_data["feature_image_src"] = image_path
-    except ElementNotVisibleException:
-        image_path = 'https://www.jpl.nasa.gov/spaceimages/images/largesize/PIA22076_hires.jpg'
-        jpl_data["feature_image_src"] = image_path
-
-    return jpl_data
-
-def scrape ():
-
-    browser = init_browser()
-    twitter_data = {}
-        
+    
     mars_weather_url = 'https://twitter.com/marswxreport?lang=en'
-    browser.visit(mars_weather_url)
-    mars_weather_html = browser.html
-    mars_weather_soup = BeautifulSoup(mars_weather_html, 'html.parser')
+    response = requests.get(mars_weather_url)
+    soup_tweet = BeautifulSoup(response.text,'html.parser')
+    recent_tweet = soup_tweet.find('p',class_='TweetTextSize').text
 
-    tweets = mars_weather_soup.find('ol', class_='stream-items')
-    mars_weather = tweets.find('p', class_="tweet-text").text
-    twitter_data["weather_summary"] = mars_weather
+    mission_to_mars['weather_tweet'] = recent_tweet
 
-    return twitter_data
-
-def scrape ():
-
-    browser = init_browser()
-    facts_data = {}
 
     mars_facts_url = 'https://space-facts.com/mars/'
-    browser.visit(mars_facts_url)
-    mars_facts_html = browser.html
-    mars_facts_soup = BeautifulSoup(mars_facts_html, 'html.parser')
+    mars_table = pd.read_html(mars_facts_url)
 
-    fact_table = mars_facts_soup.find('table', class_='tablepress tablepress-id-mars')
-    column1 = fact_table.find_all('td', class_='column-1')
-    column2 = fact_table.find_all('td', class_='column-2')
+    mars_table_df = mars_table[0]
+    mars_table_df.columns = ['Fact','Values']
 
-    facets = []
-    values = []
+    mars_table_html = mars_table_df.to_html(header=True, index=False)
+    mission_to_mars['mars_facts_table'] = mars_table_html
 
-    for row in column1:
-        facet = row.text.strip()
-        facets.append(facet)
-        
-    for row in column2:
-        value = row.text.strip()
-        values.append(value)
-        
-    mars_facts = pd.DataFrame({
-        "Facet":facets,
-        "Value":values
-        })
-
-    mars_facts_html = mars_facts.to_html(header=False, index=False)
-    facts_data["fact_table"] = mars_facts_html
-
-    return facts_data
-
-def scrape ():
-
-    browser = init_browser()
-    hemisphere_data = {}
 
     mars_hemisphere_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-    hemi_dicts = []
+    browser.visit(mars_hemisphere_url)
+    html = browser.html
+    mars_soup = BeautifulSoup(html,'html.parser')
+    hemisphere = mars_soup.find('div',class_='collapsible results')
 
-    for i in range(1,9,2):
-        
-        browser.visit(mars_hemisphere_url)
-        time.sleep(1)
-        hemispheres_html = browser.html
-        hemispheres_soup = BeautifulSoup(hemispheres_html, 'html.parser')
-        hemi_name_links = hemispheres_soup.find_all('a', class_='product-item')
-        hemi_name = hemi_name_links[i].text.strip('Enhanced')
-        
-        detail_links = browser.find_by_css('a.product-item')
-        detail_links[i].click()
-        time.sleep(1)
-        browser.find_link_by_text('Sample').first.click()
-        time.sleep(1)
-        browser.windows.current = browser.windows[-1]
-        hemi_img_html = browser.html
-        browser.windows.current = browser.windows[0]
-        browser.windows[-1].close()
-        
-        hemi_img_soup = BeautifulSoup(hemi_img_html, 'html.parser')
-        hemi_img_path = hemi_img_soup.find('img')['src']
+    results = hemisphere.find('a')
 
-        hemi_dict['title'] = hemi_name.strip()       
-        hemi_dict['img_url'] = hemi_img_path
 
-        hemi_dicts.append(hemi_dict)
+    hemisphere_list = []
 
-    hemishphere_data["hemisphere_imgs"] = hemi_dicts
+    for result in results:
+        if result.h3:
+            title = result.h3
+            link = 'https://astrogeology.usgs.gov'
+            print(title,link)    
+            browser.visit(link)
+            time.sleep(5)
+            image_html = browser.html
+            soup_scrape = BeautifulSoup(image_html,'html.parser') + result['href']
+            soup_image = soup_scrape.find('div', class_='downloads').find('li').a['href']
+            print(soup_image)
+            mars_images = {'title':title, 'img_url':soup_image}
+            hemisphere_list.append(mars_images)
 
-    return hemishphere_data
+            print(hemisphere_list)
+
+            #Cerberus
+            cerberus = hemisphere_list[0]['img_url']
+            mission_to_mars['cerberus'] = cerberus
+            #Shciaparelli
+            schiaparelli = hemisphere_list[1]['img_url']
+            mission_to_mars['schiaparelli'] = schiaparelli
+            #Syrtis Major
+            syrtis_major = hemisphere_list[2]['img_url']
+            mission_to_mars['syrtis_major'] = syrtis_major
+            #Valles Marineris
+            valles_marineris = hemisphere_list[3]['img_url']
+            mission_to_mars['valles_marineris'] = valles_marineris
+
+
+    return mission_to_mars
